9d3b48663848497cc22a4acc7cc7579919738fd1
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-01-16 14:37:41 +0100

    Add icon for admin login

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,6 +15,20 @@ const HeartIcon = (
         <path d='M13.35 20.13c-.76.69-1.93.69-2.69-.01l-.11-.1C5.3 15.27 1.87 12.16 2 8.28c.06-1.7.93-3.33 2.34-4.29 2.64-1.8 5.9-.96 7.66 1.1 1.76-2.06 5.02-2.91 7.66-1.1 1.41.96 2.28 2.59 2.34 4.29.14 3.88-3.3 6.99-8.55 11.76l-.1.09z' />
     </svg>
 );
+const AddIcon = (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        height='24'
+        viewBox='0 0 24 24'
+        width='24'
+    >
+        <g>
+            <path d='M4,6h16c0.55,0,1,0.45,1,1v4h2V6c0-1.1-0.9-2-2-2H3C1.9,4,1,4.9,1,6v12c0,1.1,0.9,2,2,2h9v-2H4c-0.55,0-1-0.45-1-1V7 C3,6.45,3.45,6,4,6z' />
+            <polygon points='15,12 9,8 9,16' />
+            <path d='M22.71,18.43c0.03-0.29,0.04-0.58,0.01-0.86l1.07-0.85c0.1-0.08,0.12-0.21,0.06-0.32l-1.03-1.79 c-0.06-0.11-0.19-0.15-0.31-0.11L21.23,15c-0.23-0.17-0.48-0.31-0.75-0.42l-0.2-1.36C20.26,13.09,20.16,13,20.03,13h-2.07 c-0.12,0-0.23,0.09-0.25,0.21l-0.2,1.36c-0.26,0.11-0.51,0.26-0.74,0.42l-1.28-0.5c-0.12-0.05-0.25,0-0.31,0.11l-1.03,1.79 c-0.06,0.11-0.04,0.24,0.06,0.32l1.07,0.86c-0.03,0.29-0.04,0.58-0.01,0.86l-1.07,0.85c-0.1,0.08-0.12,0.21-0.06,0.32l1.03,1.79 c0.06,0.11,0.19,0.15,0.31,0.11l1.27-0.5c0.23,0.17,0.48,0.31,0.75,0.42l0.2,1.36c0.02,0.12,0.12,0.21,0.25,0.21h2.07 c0.12,0,0.23-0.09,0.25-0.21l0.2-1.36c0.26-0.11,0.51-0.26,0.74-0.42l1.28,0.5c0.12,0.05,0.25,0,0.31-0.11l1.03-1.79 c0.06-0.11,0.04-0.24-0.06-0.32L22.71,18.43z M19,19.5c-0.83,0-1.5-0.67-1.5-1.5s0.67-1.5,1.5-1.5s1.5,0.67,1.5,1.5 S19.83,19.5,19,19.5z' />
+        </g>
+    </svg>
+);
 const GithubIcon = (
     <svg
         xmlns='http://www.w3.org/2000/svg'
@@ -31,20 +45,32 @@ function Footer() {
 
     return (
         <div className='flex flex-row items-center justify-center py-8'>
-            <div className='w-7 h-7' />
+            <div className='w-12 h-5' />
             <div className='self-stretch flex-grow' />
             <div className='flex flex-row items-center self-center justify-center text-gray-800'>
                 <span>built with</span>
-                <div className='w-5 h-5 mx-1 text-red-400'>{HeartIcon}</div>
+                <div className='w-5 h-5 mx-1 text-red-400' title='love'>
+                    {HeartIcon}
+                </div>
                 <span>in munich</span>
             </div>
             <div className='self-stretch flex-grow' />
             <a
+                href='https://tack-and-gybe-cms-kv6v35vima-ew.a.run.app/admin'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='mr-2'
+                title='Admin Login'
+            >
+                <div className={`w-5 h-5 ${linkColors}`}>{AddIcon}</div>
+            </a>
+            <a
                 href='https://github.com/i14sailing/tack-and-gybe-frontend'
                 target='_blank'
                 rel='noopener noreferrer'
+                title='Code on GitHub'
             >
-                <div className={`w-7 h-7 ${linkColors}`}>{GithubIcon}</div>
+                <div className={`w-5 h-5 ${linkColors}`}>{GithubIcon}</div>
             </a>
         </div>
     );
