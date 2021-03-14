19bf88214cdd141b29a703f09e16780d96de21e7
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-03-14 03:26:27 +0100

    Add netlify badge, ad new strapi api endpoint

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
             resolve: 'gatsby-source-strapi',
             options: {
                 apiURL: process.env.API_URL || 'http://localhost:1337',
-                contentTypes: ['video-submission'],
+                contentTypes: ['video-submission', 'team-submission'],
                 queryLimit: 1000,
             },
         },
