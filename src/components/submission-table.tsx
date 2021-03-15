f5d0295f036c1fce66fbde2c467303839bd74a6d
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-03-15 02:34:37 +0100

    Add flags to table and improve table design

--- src/components/submission-table.tsx
+++ src/components/submission-table.tsx
@@ -15,27 +15,60 @@ function TableRow(props: { submission: RenderSubmissionNode; index: number }) {
     return (
         <div
             className={
-                'w-full grid grid-cols-8 p-2 ' +
-                'text-base ' +
+                'w-full grid grid-cols-16 p-2 ' +
+                'text-base border-gray-300 ' +
                 (props.index !== 0 ? 'border-t-2 ' : ' ')
             }
         >
-            <div className='col-span-1 text-center font-weight-500'>
+            <div
+                className={
+                    'w-full col-span-1 font-weight-600 text-lg ' +
+                    'flex flex-row items-center justify-center'
+                }
+            >
                 {props.index + 1}
             </div>
-            <div className='col-span-1 text-center font-weight-500'>
-                {props.submission.country}
+            <div
+                className={
+                    'w-full col-span-2 font-weight-500 ' +
+                    'flex flex-row items-center justify-end'
+                }
+            >
+                <img
+                    className='rounded-sm w-14 h-7 ring-2 ring-gray-300'
+                    src={`./flags/${props.submission.country}_Flag_200w.png`}
+                />
             </div>
-            <div className='col-span-1 text-center font-weight-500'>
+            <div
+                className={
+                    'col-span-2 pl-3 font-weight-600 ' +
+                    ' flex flex-row items-center justify-start '
+                }
+            >
                 {props.submission.country} {props.submission.sailNumber}
             </div>
-            <div className='col-span-3 text-center font-weight-500'>
+            <div
+                className={
+                    'col-span-7 font-weight-600 ' +
+                    ' flex flex-row items-center justify-start '
+                }
+            >
                 {props.submission.boatsName}
             </div>
-            <div className='col-span-1 text-center font-weight-500'>
+            <div
+                className={
+                    'col-span-2 font-weight-500 ' +
+                    ' flex flex-row items-center justify-start '
+                }
+            >
                 {props.submission.time} (+ {props.submission.diff})
             </div>
-            <div className='col-span-1 text-center font-weight-500'>
+            <div
+                className={
+                    'col-span-2 font-weight-500 ' +
+                    ' flex flex-row items-center justify-start '
+                }
+            >
                 {props.submission.startTime} - {props.submission.endTime}
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,9 @@ module.exports = {
     },
     theme: {
         extend: {
+            gridTemplateColumns: {
+                '16': 'repeat(16, minmax(0, 1fr))',
+            },
             colors: {
                 transparent: 'transparent',
                 current: 'currentColor',
